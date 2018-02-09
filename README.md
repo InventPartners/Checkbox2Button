af1f4182211c528a43d182490b27a8d2eb45755a
Matthew Bradley <matt@inventpartners.com>
2018-02-09 12:13:16 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Checkbox2Button
+By Invent Partners [http://inventpartners.com]
+
+Checkbox2Button converts checkboxes and radio inputs to user-friendly buttons, whilst ensuring that the correct values are posted back to the form.
+
+### LICENSE
+
+> Copyright 2018 Invent Partners
+> http://www.inventpartners.com
+>
+> Licensed under an MIT style license
+>
+> http://www.inventpartners.com/mit-license
+
+## Demo
+Check out the [demo](https://inventpartners.github.io/Checkbox2Button/).
+
+## Requirements
+Bootstrap Responsive Tabs requires [Bootstrap](http://getbootstrap.com/) v 3.3.7 or 4.0 (CSS and JavaScript) and [jQuery](https://jquery.com/).
+
+## Usage
+Include the JavaScript and CSS files.
+
+Add a the class "checkbox2button" to each "checkox" or "form-check" div which you wish to see converted to buttons.
+
+View the [demo page](https://inventpartners.github.io/Checkbox2Button/) for further guidance on the markup etc.
+
+More details here: [http://www.inventpartners.com/open-source/checkbox2button]
+
---
+++ js/checkbox2button.js
@@ -0,0 +1,100 @@
+var checkbox2button = {
+
+	init: function(selector){	
+		$(selector).each(function(){
+			var $parent = $(this);
+			var $input = $parent.find('input[type="checkbox"]');
+			if($input.length == 1){
+				var value = $input.attr('value');
+				var name = $input.attr('name');
+				var checked = false;
+				if($input.attr('checked')){
+					checked = true;
+				}
+				var html = '<span><input type="hidden" name="' + name + '" value="" />';
+				html += '<a class="btn btn-default btn-sm btn-checkbox2btn" data-value="' + value + '"><span class="glyphicon glyphicon-unchecked"></span> ' + $parent.text() + '</a></span>';
+				$parent.html(html);
+				if(checked){
+					checkbox2button.checkCheckbox($parent.find('a'));
+				}
+			}
+			var $input = $parent.find('input[type="radio"]');
+			if($input.length == 1){
+				var value = $input.attr('value');
+				var name = $input.attr('name');
+				var checked = false;
+				if($input.attr('checked')){
+					checked = true;
+				}
+				var html = '';
+				if(!$('input[type="hidden"][name="' + name + '"]').length){
+					html = '<input type="hidden" name="' + name + '" value="" />';
+				}
+				html += '<a class="btn btn-default btn-sm btn-radio2btn" data-name="' + name + '" data-value="' + value + '"><span class="glyphicon glyphicon-unchecked"></span> ' + $parent.text() + '</a>';
+				$parent.html(html);
+				if(checked){
+					checkbox2button.checkRadioCheckbox($parent.find('a'));
+				}
+			}
+		});
+	
+		$('body').on('click' , '.btn-checkbox2btn' , function(event){
+			event.preventDefault();
+			if($(event.target).hasClass('btn-checkbox-checked')){
+				checkbox2button.unCheckCheckbox($(event.target));
+			} else {
+				checkbox2button.checkCheckbox($(event.target));
+			}
+			return false;
+		});
+	
+		$('body').on('click' , '.btn-radio2btn' , function(event){
+			event.preventDefault();
+			if($(event.target).hasClass('btn-checkbox-checked')){
+				checkbox2button.unCheckRadioCheckbox($(event.target));
+			} else {
+				checkbox2button.checkRadioCheckbox($(event.target));
+			}
+			return false;
+		});
+		
+	} ,
+
+	checkCheckbox: function (element){
+		$(element).siblings('input').val($(element).attr('data-value'));
+		$(element).addClass('btn-checkbox-checked');
+		$(element).find('.glyphicon').removeClass('glyphicon-unchecked');
+		$(element).find('.glyphicon').addClass('glyphicon-check');
+	} ,
+	
+	unCheckCheckbox: function (element){
+		$(element).siblings('input').val('');
+		$(element).removeClass('btn-checkbox-checked');
+		$(element).find('.glyphicon').addClass('glyphicon-unchecked');
+		$(element).find('.glyphicon').removeClass('glyphicon-check');
+	} ,
+
+	checkRadioCheckbox: function (element){
+		$('.btn-radio2btn[data-name="' + $(element).attr('data-name') + '"]').removeClass('btn-checkbox-checked');
+		$('.btn-radio2btn[data-name="' + $(element).attr('data-name') + '"] .glyphicon').removeClass('glyphicon-uncheck');
+		$('.btn-radio2btn[data-name="' + $(element).attr('data-name') + '"] .glyphicon').addClass('glyphicon-unchecked');
+		$('input[type="hidden"][name="' + $(element).attr('data-name') + '"]').val($(element).attr('data-value'));
+		$(element).addClass('btn-checkbox-checked');
+		$(element).find('.glyphicon').removeClass('glyphicon-unchecked');
+		$(element).find('.glyphicon').addClass('glyphicon-check');
+	} ,
+
+	unCheckRadioCheckbox: function (element){
+		$('.btn-radio2btn[data-name="' + $(element).attr('data-name') + '"]').removeClass('btn-checkbox-checked');
+		$('.btn-radio2btn[data-name="' + $(element).attr('data-name') + '"] .glyphicon').removeClass('glyphicon-uncheck');
+		$('.btn-radio2btn[data-name="' + $(element).attr('data-name') + '"] .glyphicon').addClass('glyphicon-unchecked');
+		$('input[type="hidden"][name="' + $(element).attr('data-name') + '"]').val('');
+	}
+
+
+}
+
+
+$('document').ready(function(){
+	checkbox2button.init('.checkbox2button');
+});
